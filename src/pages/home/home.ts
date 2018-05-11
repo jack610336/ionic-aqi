177e06ead14f35a1b21708aff221029feced1c3e
Jack <Jack_Wang@asus.com>
2018-05-11 14:00:37 +0800

    need to fix some issue

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,60 +8,65 @@ import 'rxjs/add/operator/map';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  items: Array<{title: string, note: string}>;
+  items: Array<{title: string, AQI: string ,colorS: string}>;
   posts: any;
-  slide: String;
+  slide: string ;
 
   constructor(public navCtrl: NavController ,public http: Http) {
  
-    this.items = [{title:"台北",note:"45"},{title:"桃園",note:"42"},{title:"北投",note:"57"},{title:"淡水",note:"36"}
-  ,{title:"中壢",note:"55"},{title:"八德",note:"62"},{title:"松山",note:"47"},{title:"士林",note:"49"}];
+  //   this.items = [{title:"台北",note:"45"},{title:"桃園",note:"42"},{title:"北投",note:"57"},{title:"淡水",note:"36"}
+  // ,{title:"中壢",note:"55"},{title:"八德",note:"62"},{title:"松山",note:"47"},{title:"士林",note:"49"}];
     this.items = [];
     // for (let i = 1; i < 11; i++) {
     //   this.items.push({title: 'Item ' + i,note:  "note : " + i  });
     // }   
-    this.posts = null;
  
     this.http.get('http://opendata2.epa.gov.tw/AQI.json').map(res => res.json()).subscribe(
       data => {
-        this.posts = String(data[1].AQI);
-
-        for (let i = 0; i < data.length; i++) {
-            this.items.push({title: data[i].SiteName ,note: data[i].AQI });
-            
-          if( data[i].AQI >=0 &&  data[i].AQI <= 50){
-            this.slide = "#00DB00";
-          }else if(data[i].AQI >50 && data[i].AQI <=100){
-            this.slide ="#FFCC00";
-          }else if(data[i].AQI > 100 && data[i].AQI <=150){
-            this.slide ="#FF8000";
-          }
-          else if(data[i].AQI > 150 && data[i].AQI <=200){
-            this.slide ="#FF0000";
-          }
-          else if(data[i].AQI >200 && data[i].AQI <=300){
-            this.slide ="#B766AD";
-          }
-          else if(data[i].AQI > 300 ){
-            this.slide ="#AE0000";
-          }
-          else {
-            this.slide ="#444444";
-          }
-        }
-        
-        
+        this.posts = data;
+        this.colorString();
       },
       err => {
         console.log("Oops!");
       });
 
      
-   
   }
 
   
 
+  colorString(){
+
+    for (let i = 0; i < this.posts.length; i++) {    
+
+      if(this.posts[i].AQI == ""){
+        this.slide ="#444444";
+        this.posts[i].AQI = "暫無資料"
+      }else if( this.posts[i].AQI >=0 &&  this.posts[i].AQI <= 50){
+        this.slide = "#00DB00";
+      }else if(this.posts[i].AQI >50 && this.posts[i].AQI <=100){
+        this.slide ="#FFCC00";
+      }else if(this.posts[i].AQI > 100 && this.posts[i].AQI <=150){
+        this.slide ="#FF8000";
+      }
+      else if(this.posts[i].AQI > 150 && this.posts[i].AQI <=200){
+        this.slide ="#FF0000";
+      }
+      else if(this.posts[i].AQI >200 && this.posts[i].AQI <=300){
+        this.slide ="#B766AD";
+      }
+      else if(this.posts[i].AQI > 300 ){
+        this.slide ="#AE0000";
+      }
+      else {
+        this.slide ="#444444";
+      }
+      this.items.push({title: this.posts[i].SiteName ,AQI: this.posts[i].AQI ,colorS: this.slide});
+
+      
+
+    }
+  }
 
   
 
